feaa84fcdcacb6dc444c29e5c5c403c47ef39fe3
Fabien Lacordelle <falaco972@icloud.com>
2023-09-26 17:46:13 +0200

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if (typeof printTimeCallback === "function") {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+        printTimeCallback(this.currentTime);
+      }, 1000);
+    } else {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let stringValue = value.toString();
+    if (stringValue.length === 1) {
+      return 0 + stringValue;
+    } else {
+      return stringValue;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+
+    {
+      if (this.intervalId !== null) {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+      }
+    }
   }
 
   split() {
